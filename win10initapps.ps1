1a3c21954d8f6736205b4276da7ad0c0598a6a0c
Levi Mota <levi.mota@gmail.com>
2020-12-25 14:53:11 -0300

    Script to istall initial applications

---
+++ win10initapps.ps1
@@ -0,0 +1,13 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install -y microsoft-windows-terminal
+choco install -y GoogleChrome
+choco install -y Firefox
+choco install -y NotepadPlusPlus
+choco install -y FoxitReader
+choco install -y adobereader
+choco install -y 7zip
+choco install -y dropbox
+choco install -y Spotify
+choco install -y git
+choco install -y vscode
+choco install -y etcher

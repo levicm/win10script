647977718e208834d5d7f3a9448c74faa80958a9
Levi Mota <levi.mota@gmail.com>
2021-01-29 15:01:16 -0300

    Update win10initapps.ps1
    Adjust

--- win10initapps.ps1
+++ win10initapps.ps1
@@ -1,4 +1,4 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y microsoft-windows-terminal
 choco install -y autohotkey
 choco install -y GoogleChrome
@@ -6,18 +6,18 @@ choco install -y Firefox
 choco install -y NotepadPlusPlus
 choco install -y FoxitReader
 choco install -y adobereader
-choco install -y office365proplus
-choco install -y 7zip
 choco install -y dropbox
-choco install -y Spotify
-choco install -y git
-choco install -y vscode
+choco install -y spotify
+choco install -y whatsapp
+choco install -y 7zip
 choco install -y etcher
 choco install -y ccleaner
 choco install -y hwinfo
 choco install -y speedtest
+choco install -y scrcpy
+choco install -y git
+choco install -y vscode
+choco install -y sketchbook
+choco install -y office365proplus
 choco install -y virtualbox
 choco install -y vmware-workstation-player
-choco install -y scrcpy
-choco install -y sketchbook
-
